6e72a72a730a369a368892797026449a957996e6
pcheropo <s1011218@stud.sbg.ac.at>
2019-03-20 10:13:16 +0100

    Add files via upload

--- Blatt01.java
+++ Blatt01.java
@@ -1,15 +1,17 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Blatt01 {
 
     public static void main(String[] args)  throws Exception{
-        int [] ref = {1,2,3};
-        //************************************//
+       
         try {
             Class.forName ( "org.postgresql.Driver" );
             System.err.println("Driver found.");
@@ -20,6 +22,11 @@ public class Blatt01 {
         }
         
         Scanner sc = new Scanner(System.in);
+        
+        System.out.println("enter 1 for straight forward \n" +
+                           "enter 2 for single Query \n" +
+                           "enter 3 for prepared statement");
+        String input = sc.nextLine();
         
         String host = "biber.cosy.sbg.ac.at";
         String port = "5432";
@@ -40,103 +47,33 @@ public class Blatt01 {
             e.printStackTrace();
             return;
         }
-        //************Iterate over different Approaches***************************//
-        for(int a = 0; a < ref.length; a++) {
             
-            String create_table = " CREATE TABLE auth(name varchar(49),pubID varchar(129));";
-            con.createStatement().execute(create_table);
+        String create_table = " CREATE TABLE auth(name varchar(49),pubID varchar(129));";
+        con.createStatement().execute(create_table);
             
-            if(ref[a] == 1) {
-                
-                System.out.println("Straightforward");
-                BufferedReader reader = new BufferedReader(new FileReader(path));
-                String line = reader.readLine();
-                String values = "";
-                String query = "";
-                String[] lineArray;
-                
-                int count = 0;
-                long start = System.currentTimeMillis();
-                while (count < 30000) {
-                    
-                    values = "";
-                    values += "('";
-                    lineArray = line.split("\t");
-                    
-                    for (int i = 0; i < lineArray.length; i++) {
-                            if (i < lineArray.length - 1) {
-                                    values += lineArray[i].replace("'", "''") + "', '";
-                            } else {
-                                    values += lineArray[i].replace("'", "''") + "');";
-                            }
-                    }
-                    
-                    query = "INSERT INTO auth VALUES" + values;
-                    
-                    con.createStatement().execute(query);
-                   
-                    line = reader.readLine();
-                    count++;  
-                    
-                }
-                
-                reader.close();
-                
-                long end = System.currentTimeMillis();
-                long runtime = end - start;
-                System.out.println("Runtime in milliseconds: " + runtime);
-            }else if(ref[a] == 2){
-                
-                System.out.println("Single Query");
-                long start = System.currentTimeMillis();
-                try {
-                        String qry = "INSERT INTO auth VALUES " + getValues(path) + ";";
-                        System.out.println("Begin transmit");
-                        con.createStatement().execute(qry);
-                        System.out.println("Query sucessful.");
-                } catch (Exception e) {
-                        System.err.println("Query was not successful.");
-                        e.printStackTrace();
-                }
-                long end = System.currentTimeMillis();
-                long runtime = end - start;
-                System.out.println("Runtime in milliseconds: " + runtime);
-                
-            }else if(ref[a] == 3){
-                int counter = 0;
-                System.out.println("Prepared Statement");
-                long start = System.currentTimeMillis();
-                BufferedReader reader = new BufferedReader(new FileReader(path));
-                String line = reader.readLine();
-                String query = "INSERT INTO auth values(?, ?);";
-                String[] cols = new String[2];
-                PreparedStatement ps = con.prepareStatement(query);
- 
-                while(counter < 30000){
-                 
-                   cols = line.split("\t");
-                   ps.setString(1, cols[0]);
-                   ps.setString(2, cols[1]);
-                   ps.addBatch();
-                   line = reader.readLine();
-                   counter ++;
-                  
-              }
-                
-              System.out.println("Begin transmit");
-              ps.executeBatch();
-        
-              reader.close(); 
-              long end = System.currentTimeMillis();
-              long runtime = end - start;
-              System.out.println("Runtime in milliseconds: " + runtime);
-            }
+        long start = System.currentTimeMillis();
             
-            String query = "DROP TABLE auth;";
-            con.createStatement().execute(query);
+        if(input.equals("1")) {
             
+            Straightforward(path,con);
+            
+        }else if(input.equals("2")){
+               
+            single_query(path,con);
+                
+        }else if(input.equals("3")){
+              
+            prepared_statement(path,con);
+                
+        }else {
+            System.out.println("invalid input");
         }
         
+        long end = System.currentTimeMillis();
+        long runtime = end - start;
+        System.out.println("Runtime in milliseconds: " + runtime);
+        String query = "DROP TABLE auth;";
+        con.createStatement().execute(query);
     }
     
    static String getValues(String path) throws Exception {
@@ -145,13 +82,12 @@ public class Blatt01 {
         BufferedReader file = new BufferedReader(new FileReader(path));
 
         String dataRow = file.readLine();
-        int count = 0;
-        while (count < 30000) {
+        
+        for (int count = 0; count < 30000; count++) {
                 dataRow = dataRow.replace("'", "''");
                 dataRow = dataRow.replace("\t", "','");
                 dataBuilder.append("('" + dataRow + "'),");
                 dataRow = file.readLine(); 
-                count++;
         }
         file.close();
 
@@ -160,5 +96,79 @@ public class Blatt01 {
 
         return data;
 }
-    
+   
+   static void Straightforward(String path,Connection con) throws IOException {
+       System.out.println("Straightforward");
+       BufferedReader reader = new BufferedReader(new FileReader(path));
+       String line = reader.readLine();
+       String values = "";
+       String query = "";
+       String[] lineArray;
+       
+       for (int count = 0; count < 30000; count++) {
+           
+           values = "";
+           values += "('";
+           lineArray = line.split("\t");
+           
+           for (int i = 0; i < lineArray.length; i++) {
+                   if (i < lineArray.length - 1) {
+                           values += lineArray[i].replace("'", "''") + "', '";
+                   } else {
+                           values += lineArray[i].replace("'", "''") + "');";
+                   }
+           }
+           
+           query = "INSERT INTO auth VALUES" + values;
+           
+           try {
+            con.createStatement().execute(query);
+           } catch (SQLException e) {
+            System.err.println("Unable to create Statement");
+            e.printStackTrace();
+           }
+          
+           line = reader.readLine();
+       }
+       
+       reader.close();
+   }
+   
+   static void single_query(String path,Connection con) {
+       System.out.println("Single Query");
+       
+       try {
+               String qry = "INSERT INTO auth VALUES " + getValues(path) + ";";
+               System.out.println("Begin transmit");
+               con.createStatement().execute(qry);
+               System.out.println("Query sucessful.");
+       } catch (Exception e) {
+               System.err.println("Query was not successful.");
+               e.printStackTrace();
+       }
+   }
+   
+   static void prepared_statement(String path,Connection con) throws IOException, SQLException {
+      
+       System.out.println("Prepared Statement");
+      
+       BufferedReader reader = new BufferedReader(new FileReader(path));
+       String line = reader.readLine();
+       String query = "INSERT INTO auth values(?, ?);";
+       String[] cols = new String[2];
+       PreparedStatement ps = con.prepareStatement(query);
+
+       for( int count = 0; count < 30000; count++){
+          cols = line.split("\t");
+          ps.setString(1, cols[0]);
+          ps.setString(2, cols[1]);
+          ps.addBatch();
+          line = reader.readLine();
+       }
+       
+       System.out.println("Begin transmit");
+       ps.executeBatch();
+
+       reader.close();
+   }
 }
